e9cfa2fbfea275b6731265abe3b3026f6a754850
Chris Kelley <chriskelley85@gmail.com>
2021-11-04 18:35:53 -0500

    thru new reactstrap components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,6 +3,20 @@ import "react-datepicker/dist/react-datepicker.css";
 import { ReadableText } from "./styled-components/ReadableText";
 import { makeStyles } from "@material-ui/core/styles";
 import Tooltip from "@material-ui/core/Tooltip";
+import { Card } from 'reactstrap';
+import { CardBody } from 'reactstrap';
+import { CardTitle } from 'reactstrap';
+import { CardSubtitle } from 'reactstrap';
+import { CardText } from 'reactstrap';
+import { Button } from 'reactstrap';
+import { Navbar } from 'reactstrap';
+import { NavbarBrand } from 'reactstrap';
+import { NavbarToggler } from 'reactstrap';
+import { Collapse } from 'reactstrap';
+import { Nav } from 'reactstrap';
+import { NavItem } from 'reactstrap';
+import { NavLink } from 'reactstrap';
+import { NavbarText } from 'reactstrap';
 
 export default class Objects extends Component {
   render() {
@@ -38,6 +52,62 @@ export default class Objects extends Component {
         {/* Description */}
         <ReadableText>{this.props.data[0].explanation}</ReadableText>
         {/* <Placeholder dates={this.state.startDate} /> */}
+        <div>
+  <Navbar
+    color="light"
+    expand="md"
+    light
+  >
+    <NavbarBrand href="/">
+      Starboy
+    </NavbarBrand>
+    <NavbarToggler onClick={function noRefCheck(){}} />
+    <Collapse navbar>
+      <Nav
+        className="me-auto"
+        navbar
+      >
+        <NavItem>
+          <NavLink href="https://chriskelley.io">
+            My Website
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/Chriskelley55">
+            My GitHub
+          </NavLink>
+        </NavItem>
+        </Nav>
+      <NavbarText>
+      “Somewhere, something incredible is waiting to be known.”
+― Carl Sagan
+      </NavbarText>
+    </Collapse>
+  </Navbar>
+</div>
+
+<div>
+  <Card
+  >
+    <CardBody>
+      <CardTitle tag="h5">
+        Galaxy
+      </CardTitle>
+      <CardSubtitle
+        className="mb-2 text-muted"
+        tag="h6"
+      >
+        Black Matter
+      </CardSubtitle>
+      <CardText>
+      Are you an astronaut, because you’re out of this world.
+      </CardText>
+      <Button>
+        Button
+      </Button>
+    </CardBody>
+  </Card>
+</div>
       </div>
     );
   }
